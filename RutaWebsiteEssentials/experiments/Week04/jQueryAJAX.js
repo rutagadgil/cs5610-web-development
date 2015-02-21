1e8f158d81d926563e71106f76d6de238f3d60f5
rutagadgil <gadgil.r@husky.neu.edu>
2015-02-21 12:12:12 -0500

    week04-completed. week-05 partially done

---
+++ RutaWebsiteEssentials/experiments/Week04/jQueryAJAX.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    $("#exp1").click(function () {
+        //alert("Inside click function");
+        $.get("../Week01/exp1-basics.html", function (data, status) {
+            var msg = data;
+
+            document.getElementById("content").innerHTML = msg;
+        });
+    });
+
+    $("#exp2").click(function () {
+        //alert("Inside click function");
+        $.get("../Week02/exp8-fluid-flow.html", function (data, status) {
+            var msg = data;
+            document.getElementById("content").innerHTML = msg;
+        });
+    });
+});
