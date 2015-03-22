6364187dfd293d3a1c72c4bb920fb4594f194736
rutagadgil <gadgil.r@husky.neu.edu>
2015-03-22 03:35:23 -0400

    week08

---
+++ RutaWebsiteEssentials/javascript/experiments_javascript/Week08/artist-on-artsy.js
@@ -0,0 +1,102 @@
+﻿function clearContents() {
+    $("#data").html("");
+}
+$(document).ready(function () {
+
+    $("#submitArtist").click(function () {
+
+        clearContents();
+        var artist = "4d8b92684eb68a1b2c00009e";
+        var url = "";
+        var url1 = "https://api.artsy.net/api/artists/";
+        var url2 = artist;
+        var url3 = "&Token:";
+        var searchURL = url1 + url2;
+        url = url1 + url2;
+        token = retrieveResults(url);
+
+    });
+
+    function retrieveResults(url) {
+
+        var tokenURL = "https://api.artsy.net/oauth2/access_token?client_id=4ca711359d8808b1614e&client_secret=2caa296b14a0239be025fc31ff6f5686&grant_type=credentials&email=gadgil.r@husky.neu.edu&password=ruts25391";
+        $.getJSON(tokenURL, function (result) {
+            token = result.access_token;
+
+            $.ajax({
+                url: url,
+                type: 'GET',
+                dataType: 'json',
+                success: function (result) {
+
+                    var div = document.createElement("div");
+                    div.setAttribute('class', 'artistData');
+                    var name_para = document.createElement("p");
+                    var name = document.createTextNode("Name: " + result.name);
+                    name_para.appendChild(name);
+                    div.appendChild(name_para);
+
+                    var dob_para = document.createElement("p");
+                    var dob = document.createTextNode("Date of birth: " + result.birthday);
+                    dob_para.appendChild(dob);
+                    div.appendChild(dob_para);
+
+                    var hometown_para = document.createElement("p");
+                    var hometown = document.createTextNode("Hometown: " + result.hometown);
+                    hometown_para.appendChild(hometown);
+                    div.appendChild(hometown_para);
+
+                    var location_para = document.createElement("p");
+                    var location = document.createTextNode("Location: " + result.location);
+                    location_para.appendChild(location);
+                    div.appendChild(location_para);
+
+                    var nationality = document.createTextNode("Nationality: " + result.nationality);
+                    var nationality_para = document.createElement("p");
+                    nationality_para.appendChild(nationality);
+                    div.appendChild(nationality_para);
+
+                    var url = result._links.artworks.href;
+                    $.ajax({
+                        url: url,
+                        type: 'GET',
+                        dataType: 'json',
+                        success: function (result) {
+                            $.each(result._embedded.artworks, function (i, results) {
+                                var para = document.createElement("p");
+                                var title = document.createTextNode("Artwork Title: " + results.title);
+                                para.appendChild(title);
+
+                                var linebreak = document.createElement("br");
+                                para.appendChild(linebreak);
+                                var img = document.createElement("img");
+                                img.setAttribute('src', results._links.thumbnail.href);
+                                para.appendChild(img);
+                                document.getElementById("data").appendChild(para);
+                            });
+                        },
+                        fail: function (result) {
+                            alert("Some failure has occurred!! Please try again later");
+                        },
+                        error: function (result) {
+                            alert("An error has occurred!! Please try again later");
+                        },
+                        beforeSend: function (jqXHR, settings) {
+                            jqXHR.setRequestHeader("X-Access-Token", token);
+                        }
+                    });
+                    document.getElementById("data").appendChild(div);
+                },
+                error: function () {
+                    alert("An error has occurred!! Please try again later");
+                },
+                fail: function () {
+                    alert("Some failure has occurred!! Please try again later");
+                },
+                beforeSend: function (jqXHR, settings) {
+                    jqXHR.setRequestHeader("X-Access-Token", token);
+                }
+            });
+        });
+    }
+});
---
+++ RutaWebsiteEssentials/javascript/experiments_javascript/Week08/artsy-search.js
@@ -0,0 +1,90 @@
+﻿function clearContents() {
+    $("#Artistcontents").html("");
+    $("#Artworkcontents").html("");
+}
+$(document).ready(function () {
+
+    $("#submitArtist").click(function () {
+
+        clearContents();
+        var artist = document.getElementById("searchString").value;
+        var url = "";
+        var url1 = "https://api.artsy.net/api/search?q=";
+        var url2 = artist;
+        var url3 = "&Token:";
+        var searchURL = url1 + url2;
+        url = url1 + url2 + url3;
+        token = retrieveResults(url);
+
+    });
+
+    function retrieveResults(url) {
+
+        var tokenURL = "https://api.artsy.net/oauth2/access_token?client_id=4ca711359d8808b1614e&client_secret=2caa296b14a0239be025fc31ff6f5686&grant_type=credentials&email=gadgil.r@husky.neu.edu&password=ruts25391";
+        $.getJSON(tokenURL, function (result) {
+            token = result.access_token;
+
+            $.ajax({
+                url: url,
+                type: 'GET',
+                dataType: 'json',
+                success: function (result) {
+                    if (result.total_count == 0) {
+                        var text = document.createTextNode("No results match the search criteria. Please try some different search data");
+                        document.getElementById("content").appendChild(text);
+                    }
+                    $.each(result._embedded.results, function (i, results) {
+                        switch (results.type) {
+                            case "Artist":
+                                var linebreak = document.createElement("br");
+                                var para = document.createElement("p");
+                                var artist = document.createElement("a");
+                                artist.innerText = results.title;
+                                artist.setAttribute('href', results._links.permalink.href);
+                                artist.setAttribute('target', '_blank');
+                                para.appendChild(artist);
+                                para.appendChild(linebreak);
+                                document.getElementById("Artistcontents").appendChild(para);
+                                break;
+
+                            case "Profile":
+                                var linebreak = document.createElement("br");
+                                var para = document.createElement("p");
+                                var artist = document.createElement("a");
+                                artist.innerText = results.title;
+                                artist.setAttribute('href', results._links.permalink.href);
+                                artist.setAttribute('target', '_blank');
+                                para.appendChild(artist);
+                                para.appendChild(linebreak);
+                                document.getElementById("Artistcontents").appendChild(para);
+                                break;
+
+                            default:
+                                var para = document.createElement("p");
+                                if (results._links.thumbnail.href != "/images/icon-152.png") {
+                                    var linebreak = document.createElement("br");
+                                    var title = document.createTextNode(results.title);
+                                    var img = document.createElement("img");
+                                    img.setAttribute('src', results._links.thumbnail.href);
+                                    para.appendChild(title);
+                                    para.appendChild(linebreak);
+                                    para.appendChild(img);
+                                }
+                                document.getElementById("Artworkcontents").appendChild(para);
+                                break;
+                        }
+                    });
+                },
+                error: function () {
+                    alert("An error has occurred!! Please try again later");
+                },
+                fail: function () {
+                    alert("Some failure has occurred!! Please try again later");
+                },
+                beforeSend: function (jqXHR, settings) {
+                    jqXHR.setRequestHeader("X-Access-Token", token);
+                }
+            });
+        });
+    }
+});
